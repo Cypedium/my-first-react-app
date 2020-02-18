f4e3d25c29e28ab30e666b45fa8d2698d6ce893b
Mikael Aurell <aurell.mikael@gmail.com>
2020-02-18 14:23:17 +0100

    My_first_react_test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextInput from "./components/TextInput";
+import Table from "./components/Table";
 
-function App() {
+
+
+class App extends Component {
+  render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
+    const name = "Mikael";
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+       <header>
+         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"></a>
+        </header>
+       <div className="container">
+         <Table />
+          <TextInput name={name} />
+        </div>
+        <footer>&copy; {name} </footer>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
